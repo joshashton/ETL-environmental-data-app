c53d5e12debbb8c1893039c759e9059354cf95dd
joshashton <joshua@ashtons.eu>
2024-06-26 15:43:22 +0100

    Streamlit Setup

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt # visualisation!
+import plotly.express as px
+import plotly.graph_objects as go
+
+## Requests is the default library for asking python to talk to the web
+import requests
+## PPrint is 'Pretty Print' Which lets us print less offensive JSON
+from pprint import pprint
+
+
+st.title("Environmental Information Hub")
+
+
